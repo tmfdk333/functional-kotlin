ee234f7f8ff786bdd61c0b34cf59918a82328443
tmfdk333 <tmfdk333@gmail.com>
2019-07-09 14:36:59 +0900

    Chapter01 클래스 - 속성1

--- src/chapter01/class.kt
+++ src/chapter01/class.kt
@@ -7,3 +7,15 @@ fun main(args: Array<String>) {
     val basic2 = VeryBasic()
 }
 */
+/*
+class BlueberryCupcake {
+    // var flavour = "블루베리"
+    val flavour = "블루베리"
+}
+
+fun main(args: Array<String>) {
+    val myCupcake = BlueberryCupcake()
+    // myCupcake.flavour = "아몬드"
+    println("내 컵케이크에는 ${myCupcake.flavour}가 있다.")
+}
+*/
