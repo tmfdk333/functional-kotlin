3404cf92d51de4e0c045a60bfbcc383a75c7b550
tmfdk333 <tmfdk333@gmail.com>
2019-07-09 14:42:00 +0900

    Chapter01 상속1

---
+++ src/chapter01/inheritance.kt
@@ -0,0 +1,21 @@
+package chapter01
+/*
+class Biscuit(val flavour: String) {
+    fun eat(): String {
+        return "냠냠냠, 맛있는 $flavour 비스킷"
+    }
+}
+open class BakeryGood(val flavour: String) {
+    fun eat(): String {
+        return "냠냠냠, 맛있는 $flavour 베이커리 제품"
+    }
+}
+
+class Cupcake(flavour: String): BakeryGood(flavour)
+class Biscuit(flavour: String): BakeryGood(flavour)
+
+fun main(args: Array<String>) {
+    val myBlueberryCupcake = Cupcake("블루베리")
+    println(myBlueberryCupcake.eat())
+}
+*/
