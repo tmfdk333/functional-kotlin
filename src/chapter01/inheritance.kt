85eaaf20e59c48462037a7a3f5b3cafcc65b8bbd
tmfdk333 <tmfdk333@gmail.com>
2019-07-09 14:59:49 +0900

    Chapter01 상속3

--- src/chapter01/inheritance.kt
+++ src/chapter01/inheritance.kt
@@ -47,3 +47,33 @@ fun main(args: Array<String>) {
     println(myBlueberryCupcake.eat())
 }
 */
+/*
+open class BakeryGood(val flavour: String) {
+    fun eat(): String {
+        return "냠냠냠, 맛있는 $flavour ${name()}"
+    }
+
+    open fun name(): String {
+        return "베이커리 제품"
+    }
+}
+
+open class Roll(flavour: String): BakeryGood(flavour) {
+    override fun name(): String {
+        return "롤"
+    }
+}
+
+class CinnamonRoll: Roll("시나몬")
+
+open class Donut(flavour: String, val topping: String): BakeryGood(flavour) {
+    override fun name(): String {
+        return "${topping}로 토핑된 도넛"
+    }
+}
+
+fun main(args: Array<String>) {
+    val myDonut = Donut("커스터드", "파우더 슈가")
+    println(myDonut.eat())
+}
+*/
